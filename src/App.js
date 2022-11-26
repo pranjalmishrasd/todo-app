196f34788118395f617e041f59a962f0764fe979
pranjalmishrasd <pranjalmishra.sd@gmail.com>
2022-11-26 19:58:23 +0530

    todo added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [task, setTask] = useState("")
+  const [complete, setComplete] = useState([])
+  const [inComplete, setInComplete] = useState([])
+
+  const addTask = () => {
+    setInComplete((tasks) => [...tasks, task]);
+    setTask("")
+  }
+
+  const removeHandler = (index) => {
+    const newTask = inComplete.filter((_, i) => i !== index)
+    setInComplete(newTask)
+  }
+
+  const completeHandler = (task, index) => {
+    const newTask = inComplete.filter((_, i) => i !== index)
+    setInComplete(newTask)
+    setComplete((tasks) => [...tasks, task]);
+  }
+  const undoHandler = (task, index) => {
+    const newTask = complete.filter((_, i) => i !== index)
+    setComplete(newTask)
+    setInComplete((tasks) => [...tasks, task]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="todo-wrapper">
+      <section className="task">
+        <input value={task}  onChange={(e) => setTask(e.target.value)} /><button disabled={!task} onClick={addTask}>Add</button>
+      </section>
+      <p>Todo</p>
+      <section className="tasks-list">
+        {inComplete.map((task, i) => {
+          return <div className="task"><span>{task}</span><button onClick={() => completeHandler(task, i)}>Complete</button> <button onClick={() => removeHandler(i)}>Remove</button></div>
+        })}
+      </section>
+      <p>Completed Task</p>
+      <section className="tasks-list">
+     
+        {complete.map((task, i) => {
+          return <div className="task"><span>{task}</span><button onClick={() => undoHandler(task, i)}>Undo</button></div>
+        })}
+      </section>
+    </div >
   );
 }
 
